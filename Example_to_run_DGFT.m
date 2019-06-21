793632ee9873af9c8e4a69fe071323e8c39a7488
rshafipour <47155940+rshafipour@users.noreply.github.com>
2019-06-20 20:36:18 -0400

    Add files via upload

---
+++ Example_to_run_DGFT.m
@@ -0,0 +1,4 @@
+clc; clear; close all
+A = [0,1,0,0,0;0,0,1,0,0;0,0,0,1,0;0,0,0,0,1;1,0,0,0,0]; % The given adjacency matrix
+num_init = 10; % number of initializations for finding the maximum frequency
+[DGFT_frequencies,DGFT_basis] = DGFT(A,10) % outputs the frequencies along with the basis
